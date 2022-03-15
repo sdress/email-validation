d1527fb89f3226dce9e4ca2a7d4609766988a176
sdress <sarah.dresser1@gmail.com>
2022-03-14 18:06:26 -0700

    adding completed controller file

--- flask_app/controllers/emails.py
+++ flask_app/controllers/emails.py
@@ -6,17 +6,24 @@ from flask_app.models.email import Email
 def show_form():
     return render_template('index.html')
 
-@app.route('/process')
+@app.route('/process', methods=['POST'])
 def process_form():
+    print(request.form['email'])
     data = {
-        'email_add': request.form('email')
+        'email_add': request.form['email']
     }
-    Email.create(data)
-    return redirect('/')
+    if not Email.validate_email(data):
+        return redirect('/')
+    # if email is valid, continue to success page
+    Email.add(data)
+    return redirect('/success')
 
-@app.route('/some-page/<int:id>')
-def show_something_else(id):
-    data = {
-        'id': id
-    }
-    return render_template('something.html', data)
+@app.route('/success')
+def show_success():
+    return render_template('success.html', all_emails=Email.get_all())
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    data = {'id': id}
+    Email.delete(data)
+    return redirect('/success')
